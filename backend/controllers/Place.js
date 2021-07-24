eb98ce28332b586c0e18df6a89316e5452acd1a7
Sanskrita Saha <sanskrita2001@gmail.com>
2021-07-24 18:52:03 +0530

    Places Routes

--- backend/controllers/Place.js
+++ backend/controllers/Place.js
@@ -2,7 +2,7 @@ const Place = require('../models/Place');
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('express-async-handler');
 
-//@desc      get all placea
+//@desc      get all places
 //@route     GET  /api/v1/places/
 //@access    Public
 exports.getPlaces = asyncHandler(async (req, res) => {
@@ -11,14 +11,26 @@ exports.getPlaces = asyncHandler(async (req, res) => {
 	res.status(200).json({ success: true, count: places.length, data: places });
 });
 
+//@desc      get single place
+//@route     GET  /api/v1/places/:id
+//@access    Public
+exports.getPlace = asyncHandler(async (req, res) => {
+	const place = await Place.findById(req.params.id);
+	if (!place)
+		throw new ErrorResponse(`Place not found with ${req.params.id}`, 404);
+	//Send a response
+	res.status(200).json({ success: true, data: place });
+});
+
 //@desc      add new place
 //@route     POST  /api/v1/places/
 //@access    Private
 exports.createPlace = asyncHandler(async (req, res) => {
-	const { name, description, location, averageRating } = req.body;
+	const { name, description, address, location, averageRating } = req.body;
 	const place = await Place.create({
 		name,
 		description,
+		address,
 		location,
 		averageRating,
 	});
@@ -27,4 +39,31 @@ exports.createPlace = asyncHandler(async (req, res) => {
 	}
 	//Send a response
 	res.status(201).json({ success: true, data: place });
+});
+
+//@desc      update place
+//@route     PUT  /api/v1/places/:id
+//@access    Private
+exports.updatePlace = asyncHandler(async (req, res) => {
+	let place = await Place.findById(req.params.id);
+	if (!place)
+		throw new ErrorResponse(`Place not found with ${req.params.id}`, 404);
+	place = await Place.findByIdAndUpdate(req.params.id, req.body, {
+		new: true,
+		runValidators: true,
+	});
+	//Send a response
+	res.status(200).json({ success: true, data: place });
+});
+
+//@desc      delete place
+//@route     DELETE  /api/v1/places/:id
+//@access    Private
+exports.deletePlace = asyncHandler(async (req, res) => {
+	let place = await Place.findById(req.params.id);
+	if (!place)
+		throw new ErrorResponse(`Place not found with ${req.params.id}`, 404);
+	place.remove();
+	//Send a response
+	res.status(200).json({ success: true, data: {} });
 });
--- backend/routes/Place.js
+++ backend/routes/Place.js
@@ -1,8 +1,16 @@
 const express = require('express');
-const { createPlace, getPlaces } = require('../controllers/Place');
+const {
+	createPlace,
+	getPlaces,
+	getPlace,
+	updatePlace,
+	deletePlace,
+} = require('../controllers/Place');
 const router = express.Router();
 
 //Add routes
 router.route('/').post(createPlace).get(getPlaces);
+
+router.route('/:id').get(getPlace).put(updatePlace).delete(deletePlace);
 
 module.exports = router;

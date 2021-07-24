a89971faf71d0e5fc14c293e54fcb88ebbc432da
Atrijo Gangopadhyay <arrijo2001@gmail.com>
2021-07-24 23:45:38 +0530

    basic react setup and components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-		<div className='App'>
-			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-				<div className='p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4'>
-					<p className='text-xl font-medium text-black'>Henlo</p>
-					<p class='text-gray-500'>You have a new message!</p>
-				</div>
-			</header>
+const App = () => {
+	return (
+		<div>
+			<h1>Hello</h1>	
 		</div>
-	);
+	)
 }
 
-export default App;
+export default App

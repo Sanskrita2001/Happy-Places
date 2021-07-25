aad04c3727583752b9b2e20b3662e28f06d87e91
Sanskrita Saha <sanskrita2001@gmail.com>
2021-07-26 02:00:11 +0530

    Subplaces Routes done

--- backend/routes/SubPlace.js
+++ backend/routes/SubPlace.js
@@ -1,8 +1,25 @@
 const express = require('express');
-const { getSubPlaces, addSubPlace } = require('../controllers/SubPlace');
+const {
+	getSubPlaces,
+	addSubPlace,
+	getSubPlace,
+	updateSubPlace,
+	deleteSubPlace,
+	subPlacePhotoUpload,
+} = require('../controllers/SubPlace');
+const upload = require('../middlewares/photoUpload');
 
 const router = express.Router({ mergeParams: true });
 
+//Add routers
 router.route('/').get(getSubPlaces).post(addSubPlace);
+
+router
+	.route('/:id')
+	.get(getSubPlace)
+	.put(updateSubPlace)
+	.delete(deleteSubPlace);
+
+router.route('/:id/photo').post(upload.single('photo'), subPlacePhotoUpload);
 
 module.exports = router;

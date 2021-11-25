6868b358846e35a5aeabadf4de52a3f986ab20ad
Sanskrita Saha <sanskrita2001@gmail.com>
2021-11-26 03:41:02 +0530

    UI changes

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -33,11 +33,11 @@ const LoginScreen = () => {
 		}
 	}, [userVerify.userInfo, history])
 	return (
-		<div className= "main-container">
+		<div className= "overflow-y-hidden">
       <img className="home-bg-img" src="https://imgur.com/oeqleuG.png" alt="Home Backgroung Image" width="100%" height="100%" />
 			<div className='min-h-screen'>
-				<div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 h-5/6 w-11/12 backdrop-filter backdrop-blur-lg rounded-3xl border-2 border-solid border-white z-10 bg-gradient-to-br from bg-grey-500 bg-opacity-60 to bg-grey-500 bg-opacity-5'>
-					<h1 className='text-center font-bold my-5'>
+				<div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 h-2/3 w-11/12 backdrop-filter backdrop-blur-lg rounded-3xl border-2 border-solid border-white z-10 bg-gradient-to-br from bg-grey-500 bg-opacity-60 to bg-grey-500 bg-opacity-5'>
+					<h1 className='text-3xl text-center font-bold my-5'>
 						Please Login to Continue
 					</h1>
 					{error && (
@@ -59,7 +59,7 @@ const LoginScreen = () => {
 								/>
 								<button
 									onClick={sendOtpHandler}
-									className='w-1/5 bg-yellow-400 m-3 border-2 border-white rounded-lg'>
+									className='text-white w-1/5 bg-dark m-3 border-2 border-white rounded-lg'>
 									SEND OTP
 								</button>
 								{loading && <Loader />}
@@ -87,8 +87,8 @@ const LoginScreen = () => {
 								</div>
 								<button
 									// onClick={sendOtp}
-									className='text-white font-bold w-1/5 bg-blue-600 m-3 border-2 border-white rounded-lg'>
-									Resend
+									className='text-white w-1/5 bg-dark m-3 border-2 border-white rounded-lg'>
+									RESEND
 								</button>
 							</div>
 						</div>
@@ -96,7 +96,7 @@ const LoginScreen = () => {
 						{userVerify.error && <h1>Some error occured</h1>}
 						<button
 							onClick={loginUser}
-							className='mt-8 w-full bg-blue-800 text-white font-bold p-4 border-2 border-white rounded-lg text-xl'>
+							className='mt-8 w-full bg-dark text-white font-bold p-4 border-2 border-white rounded-lg text-xl'>
 							Verify
 						</button>
 					</div>
